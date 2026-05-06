30510c7cd51eed53105dd0cc815e02dfdecb3ba0
Phuc Le <phuc.le.wx@renesas.com>
2026-05-06 16:33:37 +0700

    [Gen5][rt_dmac]: Add workaround for temporarily limitation of rt-dmac
    Signed-off-by: Phuc Le <phuc.le.wx@renesas.com>

--- FreeRTOS/Demo/R-Car_Gen5_CR52/drivers/dmac/rdmac_ctrl.c
+++ FreeRTOS/Demo/R-Car_Gen5_CR52/drivers/dmac/rdmac_ctrl.c
@@ -10,6 +10,7 @@
  #include <stdbool.h>
  #include <stdio.h>
  #include "dmac_ctrl_common.h"
+ #include "board.h"
  
  extern int printf_delay(const char *format, ...);
  
@@ -53,7 +54,45 @@
  {
      uint8_t Value;
      uint16_t ret = drv_OK;
- 
+
+    #if (BOARD == X5H_IRONHIDE)
+    /* Workaround for temporarily limitation of rt-dmac.
+     * Workaround: transfer unit must be 16 bytes or less.
+     */
+    if ((cfg->mTransferUnit == DRV_RTDMAC_TRANS_UNIT_32BYTE) || (cfg->mTransferUnit == DRV_RTDMAC_TRANS_UNIT_64BYTE) ||
+        (cfg->mTransferUnit == DRV_RTDMAC_TRANS_UNIT_MAX))
+    {
+        printf("RTDMAC error: transfer unit must be 16 bytes or less\n");
+        return drv_FAIL;
+    }
+
+    /* Workaround for temporarily limitation of rt-dmac.
+     * Workaround: source address must be 16-byte aligned to avoid 16-byte boundary crossing.
+     */
+    /* Workaround for no descriptor mode */
+    if (cfg->mDMAMode == DRV_DMAC_DMA_NO_DESCRIPTOR)
+    {
+        if ((cfg->mSrcAddr & 0xFU) != 0U)
+        {
+            printf("RTDMAC error: source address 0x%08x must be 16-byte aligned\n", cfg->mSrcAddr);
+            return drv_FAIL;
+        }
+    }
+    /* Workaround for descriptor mode */
+    else
+    {
+        rDmacDescMemCfg_t *p_desc_table = (rDmacDescMemCfg_t *)(uintptr_t)descCfg->mDescBaseAddr; // descriptor table base address
+        for (int i = 0; i < descCfg->mDescCount; i++)
+        {
+            if ((p_desc_table[i].SAR & 0xFU) != 0U)
+            {
+                printf("RTDMAC error: descriptor[%d].SAR=0x%08x must be 16-byte aligned\n", i, p_desc_table[i].SAR);
+                return drv_FAIL;
+            } 
+        }
+    }
+    #endif
+
      Value = R_DMAC_RcarDmacExec(dev, ch, cfg, descCfg);
      if (Value) {
          printf_delay("RTDMAC execution failed\n");
