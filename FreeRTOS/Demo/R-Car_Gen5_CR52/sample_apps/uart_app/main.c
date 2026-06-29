1bd4a631c3ab4671e590a961a150cfeebc34e3ad
Hoa Ngo <hoa.ngo.px@bp.renesas.com>
2026-06-29 18:02:59 +0700

    [Gen5][App]: Refactor and add multiple consoles test cases for uart-app
    Signed-off-by: Hoa Ngo <hoa.ngo.px@bp.renesas.com>

--- FreeRTOS/Demo/R-Car_Gen5_CR52/sample_apps/uart_app/main.c
+++ FreeRTOS/Demo/R-Car_Gen5_CR52/sample_apps/uart_app/main.c
@@ -35,11 +35,40 @@
 #include "scif.h"
 #include "interrupts.h"
 #include "stdio.h"
+#include "string.h"
 #include "serial/r_serial.h"
 #include "pfc/r_pfc_api.h"
 #include "device_tree_x5h.h"
+#include "board.h"
 #define main_LOG_TASK_PRIORITY        ( tskIDLE_PRIORITY + 1 )
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 
+#if (BOARD == X5H_RFS2)
+    uint8_t serialChannelArr[] = {
+        SCIF1,
+        SCIF0,
+        HSCIF0,
+        SCIF3,
+        HSCIF1,
+        HSCIF2
+    };
+#elif (BOARD == AI_ACC)
+    uint8_t serialChannelArr[] = {
+        SCIF1,
+        SCIF0,
+        HSCIF0,
+        HSCIF1
+    };
+#elif (BOARD == X5H_IRONHIDE)
+    uint8_t serialChannelArr[] = {
+        SCIF1,
+        HSCIF0
+    };
+#else //MDP board
+    uint8_t serialChannelArr[] = {
+        HSCIF0
+    };
+#endif
 /*-----------------------------------------------------------*/
 
 /*
@@ -51,8 +80,11 @@ static void prvLogTask( void *pvParameters );
 
 static void UartIrqTriggerTask(void *pvParameters);
 static void UARTInterruptHandler(void *data);
+static void uartIDtoString(uint8_t uartID,unsigned char *uartName);
+static void uartAppExample(uint8_t *channelArr, uint8_t arrLength);
 SemaphoreHandle_t xSemaphore = NULL;
 unsigned char p_char;
+uint8_t tcNum = 1;
 /*-----------------------------------------------------------*/
 uint32_t getIrqID(uint8_t uart_id)
 {
@@ -89,6 +121,137 @@ uint32_t getIrqID(uint8_t uart_id)
     return irqID;
 }
 
+static void uartIDtoString(uint8_t uartID, unsigned char *uartName)
+{
+    memset(uartName,0,strlen(uartName));
+
+    switch(uartID) {
+            case SCIF1:
+                strcpy(uartName, "SCIF1");
+                break;
+            case SCIF0:
+                strcpy(uartName, "SCIF0");
+                break;
+            case HSCIF0:
+                strcpy(uartName, "HSCIF0");
+                break;
+            case SCIF2_UNSUPPORTED:
+                strcpy(uartName, "SCIF2");
+                break;
+            case SCIF3:
+                strcpy(uartName, "SCIF3");
+                break;
+            case SCIF4:
+                strcpy(uartName, "SCIF4");
+                break;
+            case HSCIF1:
+                strcpy(uartName, "HSCIF1");
+                break;
+            case HSCIF2:
+                strcpy(uartName, "HSCIF2");
+                break;
+            case HSCIF3:
+                strcpy(uartName, "HSCIF3");
+                break;
+            default:
+                break;
+    }
+}
+
+static void uartAppExample(uint8_t *channelArr, uint8_t arrLength)
+{
+    uint8_t loopIndex = 0;
+    unsigned char uartNameBuf[24];
+    uint8_t allChannel[] = {
+        SCIF0,
+        SCIF1,
+        SCIF2_UNSUPPORTED,
+        SCIF3,
+        SCIF4,
+        HSCIF0,
+        HSCIF1,
+        HSCIF2,
+        HSCIF3
+    };
+
+    // --- TC 1: Support Log Test ---
+    printf("**********************************************\r\n");
+    printf("TEST CASE %d: Support log test\n", tcNum);
+    printf(">>> TC%d result: PASS. <<<\r\n", tcNum);
+    // --- TC 2: Disable Log Test ---
+    printf("**********************************************\r\n");
+    printf("TEST CASE %d: Silent console test\n", ++tcNum);
+    printf("MSG: Unexpect log out below (Silent for 8s)\n");
+
+    R_SERIAL_SetLogState(LOG_OFF);
+    for (loopIndex = 0; loopIndex < 4; loopIndex++)
+    {
+        printf("This message should NOT appear. TC%d result: FAIL.\n", tcNum);
+        vTaskDelay(2000);
+    }
+    R_SERIAL_SetLogState(LOG_ON);
+    printf(">>> Logs re-enabled. TC%d result: PASS. <<<\n\n",tcNum);
+
+    // --- TC: Multi-channel Test ---
+    for (uint8_t setIndex = 0; setIndex < ARRAY_SIZE(allChannel); setIndex++) {
+
+        if(allChannel[setIndex] == serialChannelArr[0]) {continue;}
+
+        uint8_t flag = 0;
+
+        for (uint8_t chnIndex = 0; chnIndex < arrLength; chnIndex++) {
+            if (allChannel[setIndex] == serialChannelArr[chnIndex]) {
+                flag = 1;
+                break;
+            }
+        }
+
+        ++tcNum;
+
+        R_SERIAL_ReConfigure(serialChannelArr[0]);
+        uartIDtoString(allChannel[setIndex],uartNameBuf);
+        printf("\n**********************************************\r\n");
+        printf("TEST CASE %d: Test %s\n", tcNum, uartNameBuf);
+        printf("\n=== Please manually switch to %s and update Terminal to 115200 baudrate on NEW COM ===\n", uartNameBuf);
+        vTaskDelay(500);
+
+        #if (BOARD == X5H_RFS2)
+        if (flag == 0 || allChannel[setIndex] == SCIF3)
+        #else
+        if (flag == 0)
+        #endif
+        {
+            /* NA TC*/
+            R_SERIAL_ReConfigure(serialChannelArr[0]);
+            uartIDtoString(serialChannelArr[0],uartNameBuf);
+            printf(">>> TC%d result: NA. <<<\r\n", tcNum, uartNameBuf);
+            vTaskDelay(500);
+        } else {
+            /* Normal TC */
+            if (R_SERIAL_ReConfigure(allChannel[setIndex]) == 0) {
+                uartIDtoString(allChannel[setIndex],uartNameBuf);
+                printf("\n**********************************************\r\n");
+                printf("TEST CASE %d: Test %s\n\n", tcNum, uartNameBuf);
+                for (loopIndex = 0; loopIndex < 12; loopIndex++)   /* Loop provides sufficient time for user to switch COM port connection */
+                {
+                    printf(">>> TC%d result: PASS. <<<\n", tcNum);
+                    vTaskDelay(1000);
+                }
+            } else {
+                for (loopIndex = 0; loopIndex < 10; loopIndex++) {
+                    printf(">>> TC%d result: FAIL. <<<\n", tcNum);
+                    vTaskDelay(1000);
+                }
+            }
+        }
+    }
+
+    tcNum += 1;
+    uartIDtoString(serialChannelArr[0],uartNameBuf);
+    R_SERIAL_ReConfigure(serialChannelArr[0]); /* Restore console to default port for the main loop */
+    printf("\n=== Returning to Default Console (%s) ===\n", uartNameBuf);
+}
+
 int main( void )
 {
     uint32_t irqID;
@@ -96,10 +259,10 @@ int main( void )
     /* Configure the hardware ready to run the demo. */
     prvSetupHardware();
 
-    irqID = getIrqID(UART_ID);
+    irqID = getIrqID(serialChannelArr[0]);
 
     if (irqID == 0) {
-        printf("Not support UART_ID = %d\n", UART_ID);
+        printf("Not support UART_ID = %d\n", serialChannelArr[0]);
     } else {
         /* Set Handler for Irq */
         Irq_SetupEntry(irqID, UARTInterruptHandler, NULL);
@@ -138,7 +301,7 @@ static void prvSetupHardware( void )
 	started. */
 	portDISABLE_INTERRUPTS();
 
-    R_SERIAL_PortInit(UART_ID);
+    R_SERIAL_PortInit(serialChannelArr[0]);
 
 	Irq_Setup();
 
@@ -152,54 +315,11 @@ static void prvLogTask( void *pvParameters )
     ( void ) pvParameters;
     unsigned char buffer[24] = "prvLogTask ...\n";
 
-    // --- TC 1: Support Log Test ---
-    printf("**********************************************\r\n");
-    printf("TEST CASE 1: Support log test\n");
-    printf(">>> TC1 PASS if you see this logs. <<<\r\n\n");
+    uartAppExample(serialChannelArr,ARRAY_SIZE(serialChannelArr));
 
-    // --- TC 2: Disable Log Test ---
-    printf("**********************************************\r\n");
-    printf("TEST CASE 2: Silent console test\n");
-    printf("MSG: Unexpect log out below (Silent for 8s)\n");
-    
-    R_SERIAL_SetLogState(LOG_OFF);
-    for (int i = 0; i < 4; i++)
-    {
-        printf("This message should NOT appear. Test FAILED if you see this logs!\n");
-        vTaskDelay(2000);
-    }
-    R_SERIAL_SetLogState(LOG_ON);
-    printf(">>> Logs re-enabled. TC2 PASS if no messages leaked during silence time. <<<\n\n");
-
-    // --- TC 3: Multi-channel Test ---
-    printf("**********************************************\r\n");
-    printf("TEST CASE 3: Test multi channel\n");
-    printf("Switching to HSCIF0. Please update Terminal to 115200 baudrate on NEW COM\n");
-    
-    if (R_SERIAL_ReConfigure(HSCIF0) == 0) 
-    {
-        for (int i = 0; i < 12; i++) /* Loop provides sufficient time for user to switch COM port connection */
-        {
-            printf(">>> TC3 PASS if you see this logs. <<<\n");
-            vTaskDelay(1000);
-        }
-    }
-    else
-    {
-        for (int i = 0; i < 10; i++) /* Loop provides sufficient time for user to switch COM port connection */
-        {
-            printf(">>> TC3 failed. <<<\n");
-            vTaskDelay(1000);
-        }
-    }
-
-    R_SERIAL_ReConfigure(UART_ID); /* Restore console to default port for the main loop (SCIF1) */
-    printf("=== All Test Cases Completed. Returning to Default Console (SCIF1) ===\n");
-    printf("<APP_END>\n");
+    printf("\nTEST CASE %d: UART Interrupt is ready - Please type to RX terminal for testing\n",tcNum);
     for( ;; )
     {
-        printf("\nUART Interrupt is ready - Please type to RX terminal for testing\n");
-        R_SERIAL_PutString(buffer, sizeof(buffer));
         vTaskDelay(3000);
     }
 }
@@ -218,10 +338,13 @@ void UARTInterruptHandler(void *data) {
 void UartIrqTriggerTask(void *pvParameters) {
     ( void ) pvParameters;
 
+    if (xSemaphoreTake(xSemaphore, portMAX_DELAY) == pdTRUE) {
+        printf("Task has been triggered by interrupt! Receive char: %c\n", p_char);
+        printf(">>> TC%d result: PASS. UART Irq triggered successfully. <<<\n\n", tcNum);
+        printf("\n<APP_END>\n");
+    }
+
     for(;;) {
-        if (xSemaphoreTake(xSemaphore, portMAX_DELAY) == pdTRUE) {
-            printf("Task has been triggered by interrupt! Receive char: %c\n", p_char);
-        }
     }
 }
 
