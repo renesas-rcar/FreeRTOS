e3dd1d0284bbb91ed761090cd37acc648eb1a968
Nhan Nguyen <nhan.nguyen.yk@bp.renesas.com>
2026-08-12 10:15:14 +0700

    [Gen5][SWDT][App]: Change indentation from tab to space
    Signed-off-by: Nhan Nguyen <nhan.nguyen.yk@bp.renesas.com>

--- FreeRTOS/Demo/R-Car_Gen5_CR52/sample_apps/swdt_app/main.c
+++ FreeRTOS/Demo/R-Car_Gen5_CR52/sample_apps/swdt_app/main.c
@@ -39,7 +39,7 @@
 
 #define main_SWDT_TASK_PRIORITY        ( tskIDLE_PRIORITY + 1 )
 #define printf_delay(fmt, ...)      \
-	vTaskDelay(1);		    \
+    vTaskDelay(1);		    \
 printf(fmt, ##__VA_ARGS__);         \
 
 #define SWDT_SWTCSRA_REG   (*(volatile uint32_t *)0x1C050004U)
@@ -57,95 +57,95 @@ static void prvSWDTTask( void *pvParameters );
 
 int main( void )
 {
-	/* Configure the hardware ready to run the demo. */
-	prvSetupHardware();
+    /* Configure the hardware ready to run the demo. */
+    prvSetupHardware();
 
 
-	xTaskCreate( prvSWDTTask, "prvSWDTTask", configMINIMAL_STACK_SIZE, NULL, main_SWDT_TASK_PRIORITY, NULL);
-	/* Start the tasks and timer running. */
-	vTaskStartScheduler();
-	for( ;; )
-	{
-	}
-	/* Don't expect to reach here. */
-	return 0;
+    xTaskCreate( prvSWDTTask, "prvSWDTTask", configMINIMAL_STACK_SIZE, NULL, main_SWDT_TASK_PRIORITY, NULL);
+    /* Start the tasks and timer running. */
+    vTaskStartScheduler();
+    for( ;; )
+    {
+    }
+    /* Don't expect to reach here. */
+    return 0;
 }
 /*-----------------------------------------------------------*/
 
 static void prvSetupHardware( void )
 {
-	/* Ensure no interrupts execute while the scheduler is in an inconsistent
-	state.  Interrupts are automatically enabled when the scheduler is
-	started. */
-	portDISABLE_INTERRUPTS();
+    /* Ensure no interrupts execute while the scheduler is in an inconsistent
+    state.  Interrupts are automatically enabled when the scheduler is
+    started. */
+    portDISABLE_INTERRUPTS();
 
-	Irq_Setup();
-	(void)pfcInitModules(getModuleConfigs());
+    Irq_Setup();
+    (void)pfcInitModules(getModuleConfigs());
 }
 
 static void prvSWDTTask( void *pvParameters )
 {
-	/* Remove compiler warning about unused parameter. */
-	( void ) pvParameters;
-	uint8_t init_timeout = 59;
-	uint8_t ping_rate = 5;
-	uint8_t ping_count = 3;
+    /* Remove compiler warning about unused parameter. */
+    ( void ) pvParameters;
+    uint8_t init_timeout = 59;
+    uint8_t ping_rate = 5;
+    uint8_t ping_count = 3;
 
 #if (BOARD == MDP_X5H_HIL) || (BOARD == MDP_AIACC_HIL)
-	vTaskDelay(6000);
+    vTaskDelay(6000);
 #endif
 
-	int timeout = 3000; // 3s
+    int timeout = 3000; // 3s
 
-	if ((SWDT_SWTCSRA_REG & (1U << 7)) != 0U)
-	{
-		printf("Waiting for SWDT to be available...\n");
+    if ((SWDT_SWTCSRA_REG & (1U << 7)) != 0U)
+    {
+        printf("Waiting for SWDT to be available...\n");
 
-		while (SWDT_SWTCSRA_REG & (1U << 7))
-		{
-			vTaskDelay(1);
-			timeout--;
-			if (timeout <= 0)
-			{
-				printf("Timeout: SWDT still busy\n");
-				for( ;; );
-			}
-		}
-	}
+        while (SWDT_SWTCSRA_REG & (1U << 7))
+        {
+            vTaskDelay(1);
+            timeout--;
+            if (timeout <= 0)
+            {
+                printf("Timeout: SWDT still busy\n");
+                for( ;; );
+            }
+        }
+    }
 
-	printf("\n=== TC1: Init Watchdog Timer ===\n");
-	if (R_SWDT_Init(init_timeout) == 0) {
-		printf("\n[INFO] Watchdog Timer initialized with timeout = %u seconds.\n", init_timeout);
-	} else {
-		printf("\n[ERROR] Failed to initialize Watchdog Timer.\n");
-	}
+    printf("\n=== TC1: Init Watchdog Timer ===\n");
+    if (R_SWDT_Init(init_timeout) == 0) {
+        printf("\n[INFO] Watchdog Timer initialized with timeout = %u seconds.\n", init_timeout);
+    } else {
+        printf("\n[ERROR] Failed to initialize Watchdog Timer.\n");
+    }
 
-	printf("\n=== TC2: Start Watchdog Timer ===\n");
-	if (R_SWDT_Start() == 0) {
-		printf("\n[INFO] Watchdog Timer started successfully.\n");
-	} else {
-		printf("\n[ERROR] Failed to start Watchdog Timer.\n");
-	}
+    printf("\n=== TC2: Start Watchdog Timer ===\n");
+    if (R_SWDT_Start() == 0) {
+        printf("\n[INFO] Watchdog Timer started successfully.\n");
+    } else {
+        printf("\n[ERROR] Failed to start Watchdog Timer.\n");
+    }
 
-	printf("\n=== TC3: Ping Watchdog Timer ===\n");
-	for (uint8_t i = 0; i < ping_count; i++) {
-		printf("[INFO] Ping %u: Set pingrate to %u seconds.\n", i + 1, ping_rate);
-		if (R_SWDT_Ping(ping_rate) != 0) {
-			printf("[ERROR] Ping failed at count %u\n", i + 1);
-		}
-	}
+    printf("\n=== TC3: Ping Watchdog Timer ===\n");
+    for (uint8_t i = 0; i < ping_count; i++) {
+        printf("[INFO] Ping %u: Set pingrate to %u seconds.\n", i + 1, ping_rate);
+        if (R_SWDT_Ping(ping_rate) != 0) {
+            printf("[ERROR] Ping failed at count %u\n", i + 1);
+        }
+    }
 
-	printf("\n=== TC4: Stop pinging to let system reset ===\n");
-	printf("\n[INFO] Ping count reached %u. No further pings — system should reset after timeout (%u seconds).\n",
-	ping_count, init_timeout);
-	printf("\n[NOTE] Waiting for system reset...\n");
+    printf("\n=== TC4: Stop pinging to let system reset ===\n");
+    printf("\n[INFO] Ping count reached %u. No further pings — system should reset after timeout (%u seconds).\n",
+    ping_count, init_timeout);
+    printf("\n[NOTE] Waiting for system reset...\n");
 
     printf("<APP_END>\n");
 
-	for( ;; )
-	{
-		vTaskDelay(1);
-	}
+    for( ;; )
+    {
+        vTaskDelay(1);
+    }
 }
 
 /*-----------------------------------------------------------*/
