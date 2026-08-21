0513e423ec9eed2955ef6f75483445a8f5369be9
Y Nguyen <y.nguyen.cj@bp.renesas.com>
2026-08-21 16:50:22 +0700

    Gen5: Memory: Update memory map for aiacc.
    Signed-off-by: Y Nguyen <y.nguyen.cj@bp.renesas.com>

--- FreeRTOS/Demo/R-Car_Gen5_CR52/common/memory_map/memory_map_ai_acc.h
+++ FreeRTOS/Demo/R-Car_Gen5_CR52/common/memory_map/memory_map_ai_acc.h
@@ -38,13 +38,7 @@ typedef struct st_memory_region {
 #define LDR_MEMORY_SIZE             0x00100000  // to 0x8E30_0000
 
 #define SHARE_CR_CA_ADDRESS         0x8E600000
-#define SHARE_CR_CA_SIZE            0x10000000  // to 0x9E60_0000
-
-#define UCIE_SHARE_ADDRESS          0x9E600000
-#define UCIE_SHARE_SIZE             0x04000000  // to 0xA2600000
-
-#define LINUX_CMA_ADDRESS_0         0xa2600000
-#define LINUX_CMA_SIZE_0            0x1DA00000  // to 0xC000_0000
+#define SHARE_CR_CA_SIZE            0x31A00000  // to 0x9E60_0000
 
 #define PERIPHERAL_START_1          0xC0000000
 #define PERIPHERAL_SIZE_1           0x40000000  // to 0x1_0000_0000
@@ -52,11 +46,9 @@ typedef struct st_memory_region {
 static const st_memory_region_t RCAR_MEMMORY_ARR[] = {
     {.type = OSAL,          .mem_addr = {.base_address = (uint32_t) OSAL_MEMORY_ADDRESS, .size = (uint32_t) OSAL_MEMORY_SIZE},    .attr = RAM_ATTR},
     {.type = SHARE_MEM,     .mem_addr = {.base_address = (uint32_t) LDR_MEMORY_ADDRESS,  .size = (uint32_t) LDR_MEMORY_SIZE},     .attr = RAM_NOCACHE_ATTR},
-    {.type = SHARE_MEM,     .mem_addr = {.base_address = (uint32_t) LINUX_CMA_ADDRESS_0, .size = (uint32_t) LINUX_CMA_SIZE_0},    .attr = RAM_NOCACHE_ATTR},
     {.type = SHARE_MEM,     .mem_addr = {.base_address = (uint32_t) SHARE_CR_CA_ADDRESS, .size = (uint32_t) SHARE_CR_CA_SIZE},    .attr = RAM_NOCACHE_ATTR},
     {.type = PERIPHERAL,    .mem_addr = {.base_address = (uint32_t) PERIPHERAL_START_0,  .size = (uint32_t) PERIPHERAL_SIZE_0},   .attr = DEVICE_ATTR},
     {.type = PERIPHERAL,    .mem_addr = {.base_address = (uint32_t) PERIPHERAL_START_1,  .size = (uint32_t) PERIPHERAL_SIZE_1},   .attr = DEVICE_ATTR},
-    {.type = SHARE_MEM,     .mem_addr = {.base_address = (uint32_t) UCIE_SHARE_ADDRESS,  .size = (uint32_t) UCIE_SHARE_SIZE},     .attr = DEVICE_ATTR}
 };
 
 #endif // MEMORY_MAP_AI_ACC_H
